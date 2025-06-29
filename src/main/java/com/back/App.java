fc5f1f5f26ddfd2f8ab26bcbcf0f9074509e4f50
eunchan96 <taegwon02@gmail.com>
2025-06-29 15:26:28 +0900

    명언 게시판 5단계

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -1,10 +1,13 @@
 package com.back;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     Scanner scanner = new Scanner(System.in);
     private int lastId = 0;
+    private final List<WiseSaying> wiseSayings = new ArrayList<>();
 
     public void run(){
         System.out.println("== 명언 앱 ==");
@@ -19,6 +22,7 @@ public class App {
                     return;
                 }
                 case "등록" -> actionWrite();
+                case "목록" -> actionList();
                 default -> System.out.println("알 수 없는 명령어입니다.");
             }
         }
@@ -26,10 +30,21 @@ public class App {
 
     private void actionWrite() {
         System.out.print("명언 : ");
-        String wiseSaying = scanner.nextLine().trim();
+        String content = scanner.nextLine().trim();
         System.out.print("작가 : ");
         String author = scanner.nextLine().trim();
 
-        System.out.println("%d번 명언이 등록되었습니다.".formatted(++lastId));
+        WiseSaying wiseSaying = new WiseSaying(++lastId, content, author);
+        wiseSayings.add(wiseSaying);
+
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.getId()));
+    }
+
+    private void actionList() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+        for (WiseSaying wiseSaying : wiseSayings.reversed()) {
+            System.out.println(wiseSaying);
+        }
     }
 }
---
+++ src/main/java/com/back/WiseSaying.java
@@ -0,0 +1,30 @@
+package com.back;
+
+public class WiseSaying {
+    private int id;
+    private String content;
+    private String author;
+
+    public WiseSaying(int id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "%d / %s / %s".formatted(id, author, content);
+    }
+}
